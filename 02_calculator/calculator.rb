6f80b2a9e8773775455548494b6c86cac9eaf9d0
Steven Rogers <steven.rogers1990@gmail.com>
2016-12-15 21:48:03 +0000

    complete test

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,31 @@
-#write your code here
+def add(x,y)
+    x + y
+end
+
+def subtract(x,y)
+    x - y
+end
+
+def sum(array)
+    array.reduce(0, :+)
+end
+
+def multiply(x,y)
+    x * y
+end
+
+def multiplyArray(array)
+    array.inject(:*)
+end
+
+def power(x,y)
+    x ** y
+end
+
+def factorial(x)
+    if x == 0
+        return 0
+    else
+    (1..x).reduce(1, :*)
+    end
+end
--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -1,38 +1,3 @@
-# # Topics
-#
-# * functions
-# * math
-# * arrays
-# * iterating/looping
-#
-# # Calculator
-#
-# you will build a simple calculator script with the following methods:
-#
-# `add` takes two parameters and adds them
-#
-# `subtract` takes two parameters and subtracts the second from the first
-#
-# `sum` takes an *array* of parameters and adds them all together
-#
-# # Warning
-#
-# You may not have enough knowledge yet to complete `sum`. You will probably
-# need to use **loops** (e.g. `while`) or **iterators** (e.g. `each`) to
-# get the tests to pass.
-#
-# # Bonus
-#
-# There are also some bonus exercises for when you finish the regular ones. The bonus round will help teach you test-driven *development*, not simply test-guided *learning*.
-#
-# Your mission, should you choose to accept it, is to write *tests* for three new methods:
-#
-# * `multiply` which multiplies two numbers together
-# * `power` which raises one number to the power of another number
-# * `[factorial](http://en.wikipedia.org/wiki/Factorial)` (check Wikipedia if you forgot your high school math).
-#
-#
-
 require "calculator"
 
 describe "add" do
@@ -73,27 +38,44 @@ describe "sum" do
   end
 end
 
-# Extra Credit Test-Driving Bonus:
-# once the above tests pass,
-# write tests and code for the following:
-
 describe "#multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+    expect(multiply(2,6)).to eq(12)
+  end
 
-  it "multiplies several numbers"
+  it "multiplies several numbers" do
+    expect(multiplyArray([1,2,3])).to eq(6)
+  end
   
 end
 
 describe "#power" do
-  it "raises one number to the power of another number"
+  
+  it "raises one number to the power of another number" do
+    expect(power(3,2)).to eq(9)
+  end
+
+  it "raises one number to the power of 0 and returns 1" do
+    expect(power(5,0)).to eq(1)
+  end
+
 end
 
-# http://en.wikipedia.org/wiki/Factorial
 describe "#factorial" do
-  it "computes the factorial of 0"
-  it "computes the factorial of 1"
-  it "computes the factorial of 2"
-  it "computes the factorial of 5"
-  it "computes the factorial of 10"
+  it "computes the factorial of 0" do
+    expect(factorial(0)).to eq(0)  
+  end
+  it "computes the factorial of 1" do
+    expect(factorial(1)).to eq(1)
+  end
+  it "computes the factorial of 2" do
+    expect(factorial(2)).to eq(2)
+  end
+  it "computes the factorial of 5" do
+    expect(factorial(5)).to eq(120)  
+  end
+  it "computes the factorial of 10" do
+    expect(factorial(10)).to eq(3628800)  
+  end
 end
