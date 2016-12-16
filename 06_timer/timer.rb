83189ac43e18216500e8b93d98a687167b702797
Steven Rogers <steven.rogers1990@gmail.com>
2016-12-16 22:54:45 +0000

    add files

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,18 @@
+
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def padded t
+    (t < 10 ? "0"+t.to_s : t).to_s
+  end
+
+  def time_string
+    padded( @seconds/(60*60)) + ":" +
+    padded((@seconds/60)%60)  + ":" +
+    padded( @seconds%60)
+  end
 end
