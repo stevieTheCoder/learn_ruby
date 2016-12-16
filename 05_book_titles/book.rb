201b525b6b26332cd12857d0e9ea2922ea4dae6b
Steven Rogers <steven.rogers1990@gmail.com>
2016-12-16 22:38:16 +0000

    complete

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+  attr_accessor :title
+  @title = ''
+
+  # Foisted from simon says with some noise added
+  def title
+    noise = %w(a an and by from in of or over the to)
+    words = @title.downcase.split(' ')
+    words.map! do |w|
+      if noise.include? w
+        w
+      else
+        w.capitalize
+      end
+    end
+    words[0].capitalize!
+    words.join(' ')
+  end
+
 end
